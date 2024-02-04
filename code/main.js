67757d61f4cc821d994ea30be1cb93f8bea86e14
Jose Sempere <jsempere.dev@gmail.com>
2024-02-04 21:32:02 +0100

    Add Permissions

--- code/main.js
+++ code/main.js
@@ -1,10 +1,39 @@
-function test(){
+var GCalURL = "calendar.google.com"
+var URL = "";
+
+function getURL(){
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-    let url = tabs[0].url;
-    document.getElementById("URL").innerText = url;
+    URL = String(tabs[0].url);
   });
+}
+
+function isGCalendar(url) {
+  if (URL.includes(GCalURL)) {
+    return true;
+  }
+  return false;
 }
 
 
 var boton = document.getElementById("botonClicar")
-boton.addEventListener('click', test)
+boton.addEventListener('click', () => {
+  document.getElementById("URL").innerText = URL;
+  console.log("Google: ", GCalURL);
+  console.log("Actual: ", URL);
+})
+
+function changeColors() {
+  rootElement = document.getElement;
+  rootElement.style.setProperty('--surface', '#27374D');
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  getURL();
+  if (!isGCalendar(URL)) {
+    document.getElementById("URL").innerText = "NOOO";
+    return 0;
+  } else {
+    document.getElementById("URL").innerText = "SIII";
+    changeColors();
+  }
+});
