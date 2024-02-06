978c63ec0ee9f45cae98a61b73c27ced441311ff
Jose Sempere <jsempere.dev@gmail.com>
2024-02-07 00:10:11 +0100

    Change --surface color

--- code/main.js
+++ code/main.js
@@ -1,39 +1,14 @@
-var GCalURL = "calendar.google.com"
-var URL = "";
+const site = window.location.hostname
 
-function getURL(){
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-    URL = String(tabs[0].url);
-  });
+const Add_Custom_Style = css => document.head.appendChild(document.createElement("style")).innerHTML = css
+
+
+// document.documentElement.style.setProperty('--surface', '#ff0000')
+
+if (site.includes("calendar.google.com")) {
+  Add_Custom_Style(`
+    :root {
+      --surface: #000000 !important;
+    }
+  `)
 }
-
-function isGCalendar(url) {
-  if (URL.includes(GCalURL)) {
-    return true;
-  }
-  return false;
-}
-
-
-var boton = document.getElementById("botonClicar")
-boton.addEventListener('click', () => {
-  document.getElementById("URL").innerText = URL;
-  console.log("Google: ", GCalURL);
-  console.log("Actual: ", URL);
-})
-
-function changeColors() {
-  rootElement = document.getElement;
-  rootElement.style.setProperty('--surface', '#27374D');
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  getURL();
-  if (!isGCalendar(URL)) {
-    document.getElementById("URL").innerText = "NOOO";
-    return 0;
-  } else {
-    document.getElementById("URL").innerText = "SIII";
-    changeColors();
-  }
-});
